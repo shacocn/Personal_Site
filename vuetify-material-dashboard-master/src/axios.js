b60397c097bde6f01ad48feb40e20b327ca646e1
Jack Gong TencentVM <shacocn@cs.ucla.edu>
2024-10-08 00:36:58 +0800

    tencent cloud settings done

--- vuetify-material-dashboard-master/src/axios.js
+++ vuetify-material-dashboard-master/src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api', // The frontend server URL with the API prefix
+  baseURL: 'http://124.222.84.196:8080/api', // The frontend server URL with the API prefix
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
