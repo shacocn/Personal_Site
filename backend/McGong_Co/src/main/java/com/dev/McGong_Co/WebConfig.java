6972c31b3538bfc7024c4971293773ab0a9d0559
Jack Gong <shacocn@cs.ucla.edu>
2024-10-08 00:51:48 +0800

    updated  IP permission for axios

--- backend/McGong_Co/src/main/java/com/dev/McGong_Co/WebConfig.java
+++ backend/McGong_Co/src/main/java/com/dev/McGong_Co/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081") // 允许的来源域名，* 表示允许所有域名
+                        .allowedOrigins("*") // 允许的来源域名，* 表示允许所有域名
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
                         .allowedHeaders("*")
                         .allowCredentials(false) // If you don’t need credentials
